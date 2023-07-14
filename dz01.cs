fa9902566cb95ce446e5de6e8ff1f862bee56248
Nikolas White <nikolodio.k@gmail.com>
2023-07-14 19:51:51 +0300

    first version

---
+++ dz01.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальный диапозон: ");
+int minRange = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальный диапозон: ");
+int maxRange = Convert.ToInt32(Console.ReadLine());
+
+double[,] array2D = GetMatrix(rows, columns, minRange, maxRange);
+
+PrintMatrix(array2D);
+
+double[,] GetMatrix(int rows, int columns, int min, int max) // Метод Создания двумерного массива
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix) // Метод печати двумерного массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(matrix[i, j], 2) + "\t");
+        }
+        Console.WriteLine();
+    }
+}

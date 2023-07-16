4461cc04da6652c2826494faae7102c82222e7c4
nikolodio <136374878+nikolodio@users.noreply.github.com>
2023-07-16 17:07:36 +0300

    Add files via upload

---
+++ dz03.cs
@@ -0,0 +1,46 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetMatrix(int m, int n, int min, int max) // Метод Создания двумерного массива
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) // Метод печати двумерного массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2D = GetMatrix(4, 4, 0, 10);
+PrintMatrix(array2D);
+
+void FindMiddleArifmetic(int[,] matrix) // Метод поиска среднего арефметического каждого столбца
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double result = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            result += matrix[i, j];
+        }
+        result = result / 4;
+        Console.Write(result + "; ");
+    }
+}
+
+FindMiddleArifmetic(array2D);
